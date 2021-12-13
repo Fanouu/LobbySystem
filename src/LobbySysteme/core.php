3e1649893d5c6e963cc4b547cffa803c1d5da7ed
Fanouu <74674330+Fanouu@users.noreply.github.com>
2021-12-13 12:50:14 +0100

    Update core.php

--- src/LobbySysteme/core.php
+++ src/LobbySysteme/core.php
@@ -133,7 +133,7 @@ class core extends PluginBase implements Listener, Cancellable{
             return true;
 
         });
-        $form->setTitle("§c- §6Server §c-");
+        $form->setTitle($this->setting->getNested("MenuNav.MenuNavUi"));
         $form->setContent("Choisissez une option! ");
         $form->addButton("§l§6> §r§cHeldia Practice §r\n §6O§bu§6v§be§6r§bt");
         $form->addButton("§l§6> §r§cFaction §r\n §20§c/§6150");
